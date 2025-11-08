0bc17a4ecafc648b2c0653232291527b38d3629c
Alexey <a03221983@gmail.com>
2025-11-08 15:01:29 +0200

    3

---
+++ src/main.js
@@ -0,0 +1,67 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchPhotosByQuery } from './js/pixabay-api';
+import { createGalleryCardTemplate } from './js/render-functions';
+
+const refs = {
+  form: document.querySelector('.js-search-form'),
+  gallery: document.querySelector('.js-gallery'),
+  loader: document.querySelector('.js-loader'),
+};
+
+const lightbox = new SimpleLightbox('.js-gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+refs.form.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+
+  const query = event.target.elements.user_query.value.trim();
+  refs.gallery.innerHTML = '';
+
+  if (!query) {
+    iziToast.warning({
+      message:
+        'Please enter a search query before submitting!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  refs.loader.classList.add('is-active');
+
+  fetchPhotosByQuery(query)
+    .then(data => {
+      if (!data.hits.length) {
+        iziToast.info({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      const markup = data.hits
+        .map(image => createGalleryCardTemplate(image))
+        .join('');
+
+      refs.gallery.innerHTML = markup;
+      lightbox.refresh();
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      refs.loader.classList.remove('is-active');
+      event.target.reset();
+    });
+}
